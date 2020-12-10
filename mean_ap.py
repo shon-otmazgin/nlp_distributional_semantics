b01f5070f6574b035fe8baf998d8e866a1c56259
shon-otmazgin <shon711@gmail.com>
2020-12-10 22:09:03 +0200

    create retrieved and relevant dicts

--- mean_ap.py
+++ mean_ap.py
@@ -1,25 +1,34 @@
 from collections import defaultdict
 
+from utils import WINDOW, SENTENCE, DEPENDENCY
+
+TOPIC = 'topic'
+SEMANTIC = 'semantic'
+
+relevant_words = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
+retrieved = defaultdict(lambda: defaultdict(list))
+
 input_file = 'annotation_output'
-
-topic_annotation = defaultdict(lambda: defaultdict(bool))
-semantic_annotation = defaultdict(lambda: defaultdict(bool))
-
-relevant_words = defaultdict(set)
-retrived_words = defaultdict(lambda: defaultdict())
 with open(input_file, 'r', encoding='utf8') as f:
     for row in f:
         values = row.rstrip('\n').split()
         if len(values) == 1:
             t_word = values[0]
-        else:
-            relevant_words[t_word].update()
+        elif values:
             for i in range(0, len(values), 3):
                 w, t_a, s_a = values[i:i+3]
-                topic_annotation[t_word][w] = True if t_a == '+' else False
-                semantic_annotation[t_word][w] = True if s_a == '+' else False
+                relevant_words[t_word][w][TOPIC] = True if t_a == '+' else False
+                relevant_words[t_word][w][SEMANTIC] = True if s_a == '+' else False
+
+            retrieved[t_word][WINDOW].append(values[0])
+            retrieved[t_word][SENTENCE].append(values[3])
+            retrieved[t_word][DEPENDENCY].append(values[6])
 
 
-
-N = len(topic_annotation['car'].values())
-print(N)
+print(relevant_words['car'].keys())
+print(len(relevant_words['car'].keys()))
+print(relevant_words['piano'].keys())
+print(len(relevant_words['piano'].keys()))
+print(retrieved['car'][WINDOW])
+print(retrieved['car'][SENTENCE])
+print(retrieved['car'][DEPENDENCY])
